3e74613752b402c28329b479761a5b7a7c5a0c49
Jaap Brasser <noreply@jaapbrasser.com>
2020-04-26 23:31:40 +0200

    Added deleteSLA method ☠

--- lib/rubrik-sdk.rb
+++ lib/rubrik-sdk.rb
@@ -125,6 +125,30 @@ class SlaDomain
             $logger.error("Not authenticated, the #{__method__.to_s} method requires you to authenticate to the Rubrik cluster first")
         end
     end
+
+    def self.deleteSLA(id:)
+        $logger.debug("Executing method '#{__method__.to_s}', method of '#{self.name.to_s}' class")
+        if defined?($rubrikConnection)
+            uristring = "https://#{$rubrikConnection['clusterName']}/api/v2/sla_domain/#{id.to_s}"
+            
+            uri = URI.parse(uristring)
+            $logger.debug("URI used: " + uri.to_s)
+
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+            
+            header = {}
+            header['Authorization'] = "Bearer #{$rubrikConnection['token']}"
+
+            result = http.delete(uri.request_uri, header)
+            $logger.debug("HTTP status code: " + result.code)
+            
+            puts result.response.body
+        else
+            $logger.error("Not authenticated, the #{__method__.to_s} method requires you to authenticate to the Rubrik cluster first")
+        end
+    end
 end
 
 class ClusterConnection
